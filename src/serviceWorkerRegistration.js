67a64cfec62022e064069948cc1e91c9a6e9fafb
carlos260382 <andres260382@gmail.com>
2022-08-27 12:01:16 -0500

    feature app2

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -64,6 +64,19 @@ export function register(config) {
   }
 }
 
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
@@ -71,7 +84,7 @@ function registerValidSW(swUrl, config) {
       registration.pushManager.getSubscription().then(async (sub) => {
         const subscriptions = await registration.pushManager.subscribe({
           userVisibleOnly: true,
-          applicationServerKey: vapidKeys.publicKey,
+          applicationServerKey: urlBase64ToUint8Array(vapidKeys.publicKey),
         });
         // se lo enviamos al backend
 
