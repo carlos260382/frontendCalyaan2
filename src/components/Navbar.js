7ef5346b23fabed7fa42605c6a85ccb9ec102e28
carlos260382 <andres260382@gmail.com>
2022-08-29 17:29:43 -0500

    feature styles/navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -98,146 +98,160 @@ function Navbar() {
     <div className={styles.all}>
       <div className={styles.container}>
         <Asider />
-        <div className={styles.logo}>
-          <NavLink to="/">
-            <picture>
-              <source srcSet={logoWhite} media="(max-width: 768px)" />
-              <source srcSet={logoWhite} media="(max-width: 575.98px)" />
+        <div className={styles.containerMenu}>
+          <div className={styles.logo}>
+            <NavLink to="/">
               <img src={logo} alt="imagen principal" />
-            </picture>
-          </NavLink>
-        </div>
+            </NavLink>
+          </div>
 
-        <div className={styles.menu}>
-          <a
-            href="https://calyaan.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h3> Inicio </h3>
-          </a>
-          <a
-            href="https://calyaan.com/empresarial/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h3>Servicios Corporativos</h3>
-          </a>
-          <a
-            href="https://calyaan.com/blog/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h3> Blog</h3>
-          </a>
-          <a
-            href="https://calyaan.com/quienes-somos/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h3> Nosotros</h3>
-          </a>
-          <a
-            href="https://calyaan.com/contactanos/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h3> Contacto</h3>
-          </a>
+          <div className={styles.menu}>
+            <a
+              href="https://calyaan.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3> Inicio </h3>
+            </a>
+            <a
+              href="https://calyaan.com/empresarial/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>Servicios Corporativos</h3>
+            </a>
+            <a
+              href="https://calyaan.com/blog/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3> Blog</h3>
+            </a>
+            <a
+              href="https://calyaan.com/quienes-somos/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3> Nosotros</h3>
+            </a>
+            <a
+              href="https://calyaan.com/contactanos/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3> Contacto</h3>
+            </a>
+          </div>
         </div>
-
-        <div className={styles.signin}>
-          {userInfo ? (
-            <div className="dropdown">
-              <NavLink to="#" className={styles.nav}>
-                {userInfo.phone} <i></i>{" "}
+        <div>
+          {!userInfo ? (
+            <div className={styles.containerBtn}>
+              <NavLink to="/register/01">
+                <div className={styles.btnRegister}>Registrarme</div>
               </NavLink>
-              <ul className="dropdown-content">
-                <li>
-                  <NavLink to="/profile">Perfil de Usuario</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/orderhistory">Historial de pedidos</NavLink>
-                </li>
-                <li>
-                  <NavLink to="#signout" onClick={signoutHandler}>
-                    Desconectar
-                  </NavLink>
-                </li>
-              </ul>
+              <NavLink to="/signin">
+                {" "}
+                <div className={styles.btn}>Iniciar sesión</div>
+              </NavLink>
             </div>
           ) : (
-            <NavLink to="/signin">
-              {" "}
-              <div className={styles.btn}>Iniciar sesión</div>
-            </NavLink>
+            ""
           )}
-          {userInfo && userInfo.isSeller && (
-            <div>
+        </div>
+
+        {userInfo ? (
+          <div className={styles.signin}>
+            {userInfo ? (
               <div className="dropdown">
-                <NavLink to="#admin" className={styles.nav}>
-                  Profesional <i></i>
+                <NavLink to="#" className={styles.nav}>
+                  {userInfo.phone} <i></i>{" "}
                 </NavLink>
                 <ul className="dropdown-content">
                   <li>
-                    <NavLink to="/servicelist/seller">Servicios</NavLink>
+                    <NavLink to="/profile">Perfil de Usuario</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/orderhistory">Historial de pedidos</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="#signout" onClick={signoutHandler}>
+                      Desconectar
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              ""
+            )}
+            {userInfo && userInfo.isSeller && (
+              <div>
+                <div className="dropdown">
+                  <NavLink to="#admin" className={styles.nav}>
+                    Profesional <i></i>
+                  </NavLink>
+                  <ul className="dropdown-content">
+                    <li>
+                      <NavLink to="/servicelist/seller">Servicios</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/turnlist">Turnos</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/orderlist/seller">Pedidos</NavLink>
+                    </li>
+                  </ul>
+                </div>
+                {/* <div>
+								<img src={userInfo.logo} alt='' className={styles.imgSeller} />
+							</div>{' '} */}
+              </div>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <NavLink to="#admin" className={styles.nav}>
+                  Admin <i></i>
+                </NavLink>
+                <ul className="dropdown-content">
+                  <li>
+                    <NavLink to="/dashboard">Dashboard</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/servicelist">Servicios</NavLink>
                   </li>
                   <li>
                     <NavLink to="/turnlist">Turnos</NavLink>
                   </li>
                   <li>
-                    <NavLink to="/orderlist/seller">Pedidos</NavLink>
+                    <NavLink to="/orderlist">Pedidos</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/userlist">Usuarios</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/support">Soporte</NavLink>
                   </li>
                 </ul>
               </div>
-              {/* <div>
-								<img src={userInfo.logo} alt='' className={styles.imgSeller} />
-							</div>{' '} */}
-            </div>
-          )}
-          {userInfo && userInfo.isAdmin && (
-            <div className="dropdown">
-              <NavLink to="#admin" className={styles.nav}>
-                Admin <i></i>
-              </NavLink>
-              <ul className="dropdown-content">
-                <li>
-                  <NavLink to="/dashboard">Dashboard</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/servicelist">Servicios</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/turnlist">Turnos</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/orderlist">Pedidos</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/userlist">Usuarios</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/support">Soporte</NavLink>
-                </li>
-              </ul>
-            </div>
-          )}
-        </div>
-        <div className={styles.container2}>
-          <div className={styles.carrito}>
-            <NavLink to="/cart">
-              <picture>
-                <source srcSet={carritoBlanco} media="(max-width: 768px)" />
-                <source srcSet={carritoBlanco} media="(max-width: 575.98px)" />
-                <img src={carrito} alt="description" />
-              </picture>
+            )}
+            {userInfo ? (
+              <div className={styles.container2}>
+                <div className={styles.carrito}>
+                  <NavLink to="/cart">
+                    <img src={carrito} alt="description" />
 
-              {cartItems.length > 0 && (
-                <span className={styles.badge}>{cartItems.length}</span>
-              )}
-            </NavLink>
+                    {cartItems.length > 0 && (
+                      <span className={styles.badge}>{cartItems.length}</span>
+                    )}
+                  </NavLink>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
-        </div>
+        ) : (
+          ""
+        )}
       </div>
       <div className={styles.container1}>
         <div className={styles.category}>
