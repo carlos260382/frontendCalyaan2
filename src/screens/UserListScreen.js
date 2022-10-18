14e0f3d4f6a577895111e37ba75b181f8aeaf1c3
carlos260382 <andres260382@gmail.com>
2022-10-18 09:23:08 -0500

    fix icon/userList

--- src/screens/UserListScreen.js
+++ src/screens/UserListScreen.js
@@ -1,89 +1,91 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteUser, listUsers } from '../actions/userActions';
-import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox';
-import { USER_DETAILS_RESET } from '../constants/userConstants';
-import styles from '../style/ServiceListScreen.module.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteUser, listUsers } from "../actions/userActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { USER_DETAILS_RESET } from "../constants/userConstants";
+import styles from "../style/ServiceListScreen.module.css";
 
 export default function UserListScreen(props) {
-	const userList = useSelector(state => state.userList);
-	const { loading, error, users } = userList;
+  const userList = useSelector((state) => state.userList);
+  const { loading, error, users } = userList;
 
-	const userDelete = useSelector(state => state.userDelete);
-	const {
-		loading: loadingDelete,
-		error: errorDelete,
-		success: successDelete,
-	} = userDelete;
+  const userDelete = useSelector((state) => state.userDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = userDelete;
 
-	const dispatch = useDispatch();
-	useEffect(() => {
-		dispatch(listUsers());
-		dispatch({
-			type: USER_DETAILS_RESET,
-		});
-	}, [dispatch, successDelete]);
-	const deleteHandler = user => {
-		if (window.confirm('Are you sure?')) {
-			dispatch(deleteUser(user._id));
-		}
-	};
-	return (
-		<div className={styles.container}>
-			<h1>Users</h1>
-			{loadingDelete && <LoadingBox></LoadingBox>}
-			{errorDelete && <MessageBox variant='danger'>{errorDelete}</MessageBox>}
-			{successDelete && (
-				<MessageBox variant='success'>
-					Usuario eliminado exitosamente
-				</MessageBox>
-			)}
-			{loading ? (
-				<LoadingBox></LoadingBox>
-			) : error ? (
-				<MessageBox variant='danger'>{error}</MessageBox>
-			) : (
-				<table className='table'>
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>NOMBRE</th>
-							<th>CORREO</th>
-							<th>ES VENDEDOR</th>
-							<th>ES ADMINISTRADOR</th>
-							<th>ACCION</th>
-						</tr>
-					</thead>
-					<tbody>
-						{users.map(user => (
-							<tr key={user._id}>
-								<td>{user._id}</td>
-								<td>{user.name}</td>
-								<td>{user.email}</td>
-								<td>{user.isSeller ? 'YES' : ' NO'}</td>
-								<td>{user.isAdmin ? 'YES' : 'NO'}</td>
-								<td>
-									<button
-										className={styles.btn}
-										onClick={() => props.history.push(`/user/${user._id}/edit`)}
-									>
-										Editar
-									</button>
-									<button
-										className={styles.btn}
-										onClick={() => deleteHandler(user)}
-									>
-										Eliminar
-									</button>
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			)}
-		</div>
-	);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(listUsers());
+    dispatch({
+      type: USER_DETAILS_RESET,
+    });
+  }, [dispatch, successDelete]);
+  const deleteHandler = (user) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(deleteUser(user._id));
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <h1>Users</h1>
+      {loadingDelete && <LoadingBox></LoadingBox>}
+      {errorDelete && <MessageBox variant="danger">{errorDelete}</MessageBox>}
+      {successDelete && (
+        <MessageBox variant="success">
+          Usuario eliminado exitosamente
+        </MessageBox>
+      )}
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NOMBRE</th>
+              <th>CORREO</th>
+              <th>TELEFONO</th>
+              <th>ES PROFESIONAL</th>
+              <th>ES ADMINISTRADOR</th>
+              <th>ACCION</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>{user.isSeller ? "YES" : " NO"}</td>
+                <td>{user.isAdmin ? "YES" : "NO"}</td>
+                <td>
+                  <button
+                    className={styles.btn}
+                    onClick={() => props.history.push(`/user/${user._id}/edit`)}
+                  >
+                    Editar
+                  </button>
+                  <button
+                    className={styles.btn}
+                    onClick={() => deleteHandler(user)}
+                  >
+                    Eliminar
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
