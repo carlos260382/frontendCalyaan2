5cc7b7fcd8d137fb5591323f0aa5ca85409d7a85
carlos260382 <andres260382@gmail.com>
2022-10-26 11:34:13 -0500

    fix chatBox

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
-import { useSelector } from "react-redux";
+//import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import ChatBox from "../components/ChatBox.js";
+//import ChatBox from "../components/ChatBox.js";
 import styles from "../style/Footer.module.css";
 import pinteres from "../assent/pinteres.png";
 import youtube from "../assent/youtube.png";
@@ -9,8 +9,8 @@ import instagram from "../assent/instagram.png";
 import logo from "../assent/logo2.png";
 
 export default function Footer() {
-  const userSignin = useSelector((state) => state.userSignin);
-  const { userInfo } = userSignin;
+  // const userSignin = useSelector((state) => state.userSignin);
+  //const { userInfo } = userSignin;
   // const goWhatsApp = () => {
   // 	window.location.replace(
   // 		'https://api.whatsapp.com/send?phone=+573127411293&text=hola'
